c9ade54c7e7d4da9efcb66b327a888e3603b44b1
code <amrkhmohammedyoussef@gmail.com>
2024-12-03 17:48:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -35,34 +35,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Start COntent Menu
 
-document.addEventListener('DOMContentLoaded', () => { 
-    const menuButton = document.getElementById('menuButton'); 
-    const menuContent = document.getElementById('menuContent'); 
-    const closeIcon = document.querySelector('.close-icon'); 
-    const spans = menuButton.querySelectorAll('span'); 
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menuButton');
+    const menuContent = document.getElementById('menuContent');
+    const closeIcon = document.querySelector('.close-icon');
+    const spans = menuButton.querySelectorAll('span');
 
-    menuButton.addEventListener('click', () => { 
+    menuButton.addEventListener('click', () => {
         menuContent.classList.toggle('active'); // تفعيل/تعطيل القائمة  
         spans.forEach(span => span.classList.add('hidden')); // إخفاء جميع عناصر span عند فتح القائمة
         document.body.classList.add('no-scroll'); // منع التمرير عند فتح القائمة
-    }); 
+    });
 
     // إغلاق القائمة عند الضغط على علامة الإغلاق
-    closeIcon.addEventListener('click', closeMenu); 
+    closeIcon.addEventListener('click', closeMenu);
 
     // لإغلاق القائمة عند الضغط خارجها  
-    document.addEventListener('click', (event) => { 
-        if (!menuContent.contains(event.target) && !menuButton.contains(event.target)) { 
-            closeMenu(); 
-        } 
-    }); 
+    document.addEventListener('click', (event) => {
+        if (!menuContent.contains(event.target) && !menuButton.contains(event.target)) {
+            closeMenu();
+        }
+    });
 
     // دالة لإغلاق القائمة  
-    function closeMenu() { 
+    function closeMenu() {
         menuContent.classList.remove('active'); // إخفاء القائمة  
         spans.forEach(span => span.classList.remove('hidden')); // إظهار جميع عناصر span
         document.body.classList.remove('no-scroll'); // إعادة التمرير
-    } 
+    }
 
     // تمكين التمرير عند الضغط على الروابط
     const menuLinks = document.querySelectorAll('.choice-in-menu a');
@@ -77,117 +77,102 @@ document.addEventListener('DOMContentLoaded', () => {
 // End Content Menu
 
 
-// Start Button Scroll Up
+// Start Slider
+const slides = document.querySelector(".slider").children;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const indicator = document.querySelector(".indicator");
+let index = 0;
 
-// إظهار الزر عند النزول للأسفل
-window.onscroll = function () {
-    var scrollBtn = document.getElementById("scrollBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollBtn.style.display = "block";
-    } else {
-        scrollBtn.style.display = "none";
+
+prev.addEventListener("click", function () {
+    prevSlide();
+    updateCircleIndicator();
+    resetTimer();
+})
+
+next.addEventListener("click", function () {
+    nextSlide();
+    updateCircleIndicator();
+    resetTimer();
+
+})
+
+// create circle indicators
+function circleIndicator() {
+    for (let i = 0; i < slides.length; i++) {
+        const article = document.createElement("article");
+        article.innerHTML = i + 1;
+        article.setAttribute("onclick", "indicateSlide(this)")
+        article.id = i;
+        if (i == 0) {
+            article.className = "active";
+        }
+        indicator.appendChild(article);
     }
-};
-// وظيفة الانتقال إلى الأعلى
-function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
 }
-// End Button Scroll Up
+circleIndicator();
 
+function indicateSlide(element) {
+    index = element.id;
+    changeSlide();
+    updateCircleIndicator();
+    resetTimer();
+}
 
-// Start Section Text Head
+function updateCircleIndicator() {
+    for (let i = 0; i < indicator.children.length; i++) {
+        indicator.children[i].classList.remove("active");
+    }
+    indicator.children[index].classList.add("active");
+}
 
-document.addEventListener("scroll", function () {
-    const paragraphs = document.querySelectorAll('.txt-head p');
-    const windowHeight = window.innerHeight;
+function prevSlide() {
+    if (index == 0) {
+        index = slides.length - 1;
+    }
+    else {
+        index--;
+    }
+    changeSlide();
+}
 
-    paragraphs.forEach((p) => {
-        const elementTop = p.getBoundingClientRect().top;
+function nextSlide() {
+    if (index == slides.length - 1) {
+        index = 0;
+    }
+    else {
+        index++;
+    }
+    changeSlide();
+}
 
-        // تحقق إذا كانت الفقرة في نطاق العرض
-        if (elementTop < windowHeight && elementTop > 0) {
-            p.style.opacity = 1; // جعل النص مرئي
-            p.style.transform = 'translateY(0)'; // إعادة موضع النص
-        }
-    });
-});
-
-// End Section Text Head
-
-
-//   Start counter-wrapper
-
-const counters = document.querySelectorAll('.counter');
-
-const animationDuration = 2000; // desired animation duration in milliseconds
-const incrementAmount = 100; // increment amount for values 200 and above
-
-counters.forEach((counter) => {
-    const target = counter.querySelector('[data-target]').dataset.target;
-    const countElement = counter.querySelector('.count');
-    let currentCount = 0;
-    let startTime;
-    let animationStarted = false;
-
-    function incrementCount() {
-        const currentTime = performance.now();
-        const elapsed = currentTime - startTime;
-        const progress = elapsed / animationDuration;
-        currentCount = Math.min(target, progress * target);
-
-        if (currentCount < 200) {
-            currentCount = Math.round(progress * target); // increment slowly for values less than 200
-        } else {
-            currentCount = Math.round(progress * target / incrementAmount) * incrementAmount; // increment by 100 for values 200 and above
-        }
-
-        countElement.textContent = currentCount + '+';
-
-        if (elapsed < animationDuration) {
-            requestAnimationFrame(incrementCount);
-        } else {
-            countElement.textContent = target + '+';
-        }
+function changeSlide() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
     }
 
-    function checkIfInView() {
-        const rect = counter.getBoundingClientRect();
-        const viewHeight = window.innerHeight;
-        const offset = 100;
+    slides[index].classList.add("active");
+}
 
-        if (rect.top <= viewHeight + offset && !animationStarted) {
-            animationStarted = true;
-            startTime = performance.now();
-            incrementCount();
-        }
-    }
-    window.addEventListener('scroll', checkIfInView);
-    checkIfInView(); // initial check
-});
-
-// End counter-wrapper
+function resetTimer() {
+    // when click to indicator or controls button 
+    // stop timer 
+    clearInterval(timer);
+    // then started again timer
+    timer = setInterval(autoPlay, 4000);
+}
 
 
-//  Start  benifit
+function autoPlay() {
+    nextSlide();
+    updateCircleIndicator();
+}
 
-// دا الانيميشن بتاع فقرات فوائد العضوية
-const benefits = document.querySelectorAll('.benefit');
-let delay = 1500; // التأخير بين كل فقرة (1.5 ثانية)
+let timer = setInterval(autoPlay, 4000);
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            benefits.forEach((benefit, index) => {
-                setTimeout(() => {
-                    benefit.classList.add('visible');
-                }, delay * index);
-            });
-            observer.disconnect(); // إيقاف المراقبة بعد التفعيل
-        }
-    });
-});
+// End Slider
 
-observer.observe(document.getElementById('benefits'));
 
-//  End  benifit
+
+
